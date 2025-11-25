1cb825f64f09afdc3bcb358172d112c8de6d4018
yoniu <1376652515@qq.com>
2025-11-25 11:49:12 +0800

    feat: 新增 TinaCMS

---
+++ tina/config.ts
@@ -0,0 +1,88 @@
+import { defineConfig } from "tinacms";
+import dayjs from 'dayjs';
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/r/content-modelling-collections/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "文章",
+        path: 'src/articles',
+        match: {
+          include: '*',
+        },
+        format: 'md',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "标题",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "内容",
+            isBody: true,
+          },
+          {
+            type: "string",
+            name: "permalink",
+            label: "永久链接",
+          },
+          {
+            type: "string",
+            name: "cover",
+            label: "封面 URL",
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "标签",
+            list: true,
+          },
+          {
+            type: "datetime",
+            name: "datePublished",
+            label: "发布日期",
+            ui: {
+              format: (value) => {
+                return dayjs(value).format("YYYY-MM-DD HH:mm:ss")
+              },
+            }
+          },
+        ],
+      },
+    ],
+  },
+
+  client: {
+    skip: true,
+  }
+});
